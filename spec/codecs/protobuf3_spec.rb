2668d19eedc5e1afe6c953ebeb32120812157f82
Inga Feick <inga.feick@trivago.com>
2018-02-17 16:17:07 +0100

    Split test cases for different protobuf versions

--- spec/codecs/protobuf3_spec.rb
+++ spec/codecs/protobuf3_spec.rb
@@ -5,12 +5,70 @@ require "logstash/event"
 require "insist"
 
 
+require 'google/protobuf' # for protobuf3
 require 'protocol_buffers' # https://github.com/codekitchen/ruby-protocol-buffers, for protobuf2
 
 
 describe LogStash::Codecs::Protobuf do
 
 
+  context "#decodePB3" do
+
+
+    #### Test case 1: Decode simple protobuf ####################################################################################################################
+    let(:plugin_unicorn) { LogStash::Codecs::Protobuf.new("class_name" => "Unicorn", "include_path" => ['spec/helpers/pb3/unicorn_pb.rb'], "protobuf_version_3" => true)  }
+    before do
+        plugin_unicorn.register      
+    end
+
+    it "should return an event from protobuf encoded data" do
+    
+      unicorn_class = Google::Protobuf::DescriptorPool.generated_pool.lookup("Unicorn").msgclass
+      data = {:name => 'Pinkie', :age => 18, :is_pegasus => false, :favourite_numbers => [4711,23], :fur_colour => Colour::PINK, 
+      :favourite_colours => [Colour::GREEN, Colour::BLUE]
+      }
+      
+      unicorn_object = unicorn_class.new(data)
+      bin = unicorn_class.encode(unicorn_object)
+      plugin_unicorn.decode(bin) do |event|
+        expect(event.get("name") ).to eq(data[:name] )
+        expect(event.get("age") ).to eq(data[:age])
+        expect(event.get("fur_colour") ).to eq(data[:fur_colour])
+        expect(event.get("favourite_numbers") ).to eq(data[:favourite_numbers] )
+        expect(event.get("favourite_colours") ).to eq(data[:favourite_colours] )
+        expect(event.get("is_pegasus") ).to eq(data[:is_pegasus] )
+      end
+    end # it
+
+
+
+
+
+    #### Test case 2: Decode without enum translation ####################################################################################################################
+    let(:plugin_unicorn) { LogStash::Codecs::Protobuf.new("class_name" => "Unicorn", "resolve_enums_to_int" => false, "include_path" => ['spec/helpers/pb3/unicorn_pb.rb'], "protobuf_version_3" => true)  }
+    before do
+        plugin_unicorn.register      
+    end
+
+    it "should return an event with enums as symbols" do
+    
+      unicorn_class = Google::Protobuf::DescriptorPool.generated_pool.lookup("Unicorn").msgclass
+      data = {:name => 'Glitter', :fur_colour => Colour::GLITTER
+      }
+      
+      unicorn_object = unicorn_class.new(data)
+      bin = unicorn_class.encode(unicorn_object)
+      plugin_unicorn.decode(bin) do |event|
+        expect(event.get("name") ).to eq(data[:name] )
+        expect(event.get("fur_colour") ).to eq("GLITTER" )
+
+      end
+    end # it
+
+
+  end # context #decodePB3
+
+  # TODO encodePB3
 
   context "#decodePB2" do
 
